a75508d97a97b07be50bd1e1bad2376c836db350
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-07-22 21:21:50 -0300

    Agregado PreguntaError y throws en Interface Pregunta.

--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -3,8 +3,8 @@ package edu.fiuba.algo3.modelo;
 import java.util.ArrayList;
 
 public interface Pregunta {
-    Integer PuntajeConOpciones(ArrayList<Opcion> opciones);
-    void Iniciar(Jugador jugador);
-    void SeleccionarOpcion(Opcion opcion);
-    Respuesta Confirmar();
+    Integer PuntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError;
+    void Iniciar(Jugador jugador) throws PreguntaError;
+    void SeleccionarOpcion(Opcion opcion) throws PreguntaError;
+    Respuesta Confirmar() throws PreguntaError;
 }
---
+++ src/main/java/edu/fiuba/algo3/modelo/PreguntaError.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public class PreguntaError extends Exception{
+    public PreguntaError(String mensaje) {
+        super(mensaje);
+    }
+}
