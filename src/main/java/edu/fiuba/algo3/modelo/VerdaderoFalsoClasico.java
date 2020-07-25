028cd449dd7a6e55a114cdab85e0321ed9cd2f40
Francisco Florit <67167048+floritfran@users.noreply.github.com>
2020-07-25 16:38:55 -0300

    Implementacion de puntajeConOpciones() provisorio

--- src/main/java/edu/fiuba/algo3/modelo/VerdaderoFalsoClasico.java
+++ src/main/java/edu/fiuba/algo3/modelo/VerdaderoFalsoClasico.java
@@ -2,10 +2,11 @@ package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
 
-public class VerdaderoFalsoClasico {
+public class VerdaderoFalsoClasico implements Pregunta {
 
     private ArrayList<Opcion> opciones;
     private String titulo;
+    private Respuesta respuestaActual;
 
     public VerdaderoFalsoClasico(String pregunta){
         this.opciones = new ArrayList<Opcion>();
@@ -17,17 +18,39 @@ public class VerdaderoFalsoClasico {
         this.opciones.add(opcion);
     }
 
-    public void agregarOpcion(String opcionTitulo) {
+    public void agregarOpcionIncorrecta(String opcionTitulo) {
         Opcion opcion = new Opcion(opcionTitulo, 0);
         this.opciones.add(opcion);
     }
 
-    public ArrayList<Integer> obtenerPuntajes(ArrayList<Opcion> todasLasRespuestas) {
+    public ArrayList<Integer> puntajeConRespuestas(ArrayList<Respuesta> todasLasRespuestas) {
         ArrayList<Integer> puntajes = new ArrayList<Integer>();
-        for(Opcion respuestaPorJugador : todasLasRespuestas){
-            puntajes.add(respuestaPorJugador.getValor());
+        for(Respuesta respuestaPorJugador : todasLasRespuestas){
+            for (Opcion opcion : respuestaPorJugador.obtenerOpcionesElegidas()){
+                puntajes.add(opcion.getValor());
+            }
         }
         return puntajes;
+    }
+
+    @Override
+    public void iniciar(Jugador jugador) throws PreguntaError {
+        this.respuestaActual = new Respuesta(this, jugador);
+    }
+
+    @Override
+    public void seleccionarOpcion(Opcion opcion) throws PreguntaError {
+        this.respuestaActual.agregarOpcion(opcion);
+    }
+
+    @Override
+    public Respuesta confirmar() throws PreguntaError {
+        return this.respuestaActual;
+    }
+
+    @Override
+    public Integer puntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError{
+        return 1;
     }
 
     public ArrayList<Opcion> obtenerOpciones() {
