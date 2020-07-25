79e93c6f4a18f420c8709a51e0fd32e3d34bfc32
Francisco Florit <67167048+floritfran@users.noreply.github.com>
2020-07-25 16:35:22 -0300

    Agrego el metodo puntajeConOpciones() provisorio

--- src/main/java/edu/fiuba/algo3/modelo/MultipleChoiseParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/MultipleChoiseParcial.java
@@ -2,10 +2,11 @@ package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
 
-public class MultipleChoiseParcial {
+public class MultipleChoiseParcial implements Pregunta {
 
     private String titulo;
     private ArrayList<Opcion> opciones = new ArrayList<Opcion>();
+    private Respuesta respuestaActual;
 
     public MultipleChoiseParcial(String titulo){
         this.titulo = titulo;
@@ -29,19 +30,39 @@ public class MultipleChoiseParcial {
         return this.opciones;
     }
 
-    public ArrayList<Integer> obtenerPuntajes(ArrayList<ArrayList<Opcion>> todasLasRespuestas) {
+    public ArrayList<Integer> puntajeConRespuestas(ArrayList<Respuesta> todasLasRespuestas) {
         ArrayList<Integer> puntajesPorJugador = new ArrayList<Integer>();
-        for (ArrayList<Opcion> respuestaDeCadaJugador : todasLasRespuestas){
+        for (Respuesta respuestaDeCadaJugador : todasLasRespuestas){
             Integer puntajeParcial = 0;
-            for (Opcion opcionElegida : respuestaDeCadaJugador){
+            for (Opcion opcionElegida : respuestaDeCadaJugador.obtenerOpcionesElegidas()){
                 puntajeParcial = puntajeParcial + opcionElegida.getValor();
             }
-            if (puntajeParcial != respuestaDeCadaJugador.size()) {
+            if (puntajeParcial != (respuestaDeCadaJugador.obtenerOpcionesElegidas()).size()) {
                 puntajesPorJugador.add(0);
             } else {
                 puntajesPorJugador.add(puntajeParcial);
             }
         }
         return puntajesPorJugador;
+    }
+
+    @Override
+    public void iniciar(Jugador jugador) throws PreguntaError {
+        this.respuestaActual = new Respuesta(this, jugador);
+    }
+
+    @Override
+    public void seleccionarOpcion(Opcion opcion) throws PreguntaError {
+        this.respuestaActual.agregarOpcion(opcion);
+    }
+
+    @Override
+    public Respuesta confirmar() throws PreguntaError {
+        return this.respuestaActual;
+    }
+
+    @Override
+    public Integer puntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError{
+        return 1;
     }
 }
