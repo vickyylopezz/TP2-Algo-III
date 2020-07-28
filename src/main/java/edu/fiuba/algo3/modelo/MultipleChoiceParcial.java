2f420936cff80e03a20b988ea0efaf06a4f0aa69
Francisco Florit <67167048+floritfran@users.noreply.github.com>
2020-07-27 21:08:21 -0300

    Actualizacion con metodos interfaz pregunta

--- src/main/java/edu/fiuba/algo3/modelo/MultipleChoiceParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/MultipleChoiceParcial.java
@@ -2,22 +2,33 @@ package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
 
-public class MultipleChoiceParcial implements Pregunta {
+public class MultipleChoiseParcial implements Pregunta {
 
     private String titulo;
-    private ArrayList<Opcion> opciones = new ArrayList<>();
+    private ArrayList<Opcion> opciones = new ArrayList<Opcion>();
     private Respuesta respuestaActual;
+    private Integer segundos;
 
-    public MultipleChoiceParcial(String titulo){
+    public MultipleChoiseParcial(String titulo, Integer segundos) throws PreguntaError {
+        if (segundos < 0){
+            throw new PreguntaError("Los segundos no pueden ser negativos");
+        }
+        this.segundos = segundos;
         this.titulo = titulo;
     }
 
-    public void agregarOpcionCorrecta(String opcionTitulo) {
+    public void agregarOpcionCorrecta(String opcionTitulo) throws PreguntaError {
+        if (this.opciones.size() == 5){
+            throw new PreguntaError("Se alcanzo el maximo de opciones para esta pregunta");
+        }
         Opcion opcion = new Opcion(opcionTitulo, 1);
         this.opciones.add(opcion);
     }
 
-    public void agregarOpcionIncorrecta(String opcionTitulo) {
+    public void agregarOpcionIncorrecta(String opcionTitulo) throws PreguntaError {
+        if (this.opciones.size() == 5){
+            throw new PreguntaError("Se alcanzo el maximo de opciones para esta pregunta");
+        }
         Opcion opcion = new Opcion(opcionTitulo, 0);
         this.opciones.add(opcion);
     }
@@ -30,39 +41,44 @@ public class MultipleChoiceParcial implements Pregunta {
         return this.opciones;
     }
 
-    public ArrayList<Integer> puntajeConRespuestas(ArrayList<Respuesta> todasLasRespuestas) {
-        ArrayList<Integer> puntajesPorJugador = new ArrayList<>();
-        for (Respuesta respuestaDeCadaJugador : todasLasRespuestas){
-            int puntajeParcial = 0;
-            for (Opcion opcionElegida : respuestaDeCadaJugador.obtenerOpcionesElegidas()){
-                puntajeParcial = puntajeParcial + opcionElegida.getValor();
-            }
-            if (puntajeParcial != (respuestaDeCadaJugador.obtenerOpcionesElegidas()).size()) {
-                puntajesPorJugador.add(0);
-            } else {
-                puntajesPorJugador.add(puntajeParcial);
-            }
-        }
-        return puntajesPorJugador;
-    }
-
     @Override
-    public void iniciar(Jugador jugador) {
+    public void iniciar(Jugador jugador) throws PreguntaError {
+        if (this.opciones.size() < 2) {
+            throw new PreguntaError("Cantidad de opciones guardadas invalida");
+        }
+        if (jugador == null) {
+            throw new PreguntaError("Jugador nulo");
+        }
+        if (this.respuestaActual != null) {
+            throw new PreguntaError("No se ha cerrado el ultimo jugador");
+        }
         this.respuestaActual = new Respuesta(this, jugador);
     }
 
     @Override
-    public void seleccionarOpcion(Opcion opcion) throws RespuestaError {
+    public void seleccionarOpcion(Opcion opcion) throws PreguntaError {
         this.respuestaActual.agregarOpcion(opcion);
     }
 
     @Override
-    public Respuesta confirmar() {
-        return this.respuestaActual;
+    public Respuesta confirmar() throws PreguntaError {
+        Respuesta resultado = this.respuestaActual;
+        this.respuestaActual = null;
+        return resultado;
     }
 
     @Override
     public Integer puntajeConOpciones(ArrayList<Opcion> opciones) {
-        return 1;
+        if (opciones.size() == 0){
+            return 0;
+        }
+        Integer puntajeParcial = 0;
+        for (Opcion opcion : opciones){
+            puntajeParcial = puntajeParcial + opcion.getValor();
+        }
+        if (puntajeParcial != opciones.size()) {
+            return 0;
+        }
+        return puntajeParcial;
     }
 }
