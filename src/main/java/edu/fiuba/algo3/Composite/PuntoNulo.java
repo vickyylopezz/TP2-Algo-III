34db1c95b13f52743bfba1f79c24c46726fbd0b9
vickyylopezz <vlopez@fi.uba.ar>
2020-07-29 14:47:00 -0300

    Patron Composite en Punto

---
+++ src/main/java/edu/fiuba/algo3/Composite/PuntoNulo.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.Composite;
+//Hoja
+public class PuntoNulo implements Punto {
+    private Integer valor = 0;
+
+    @Override
+    public Integer getValor() {
+        return this.valor;
+    }
+}
