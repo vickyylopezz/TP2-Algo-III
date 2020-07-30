4bf723e2961a190f0528f34b05a44891987778eb
vickyylopezz <vlopez@fi.uba.ar>
2020-07-30 15:29:05 -0300

    Cambio interface Pregunta para que puntajeConOpciones devuelva Punto y no Integer

--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -1,9 +1,11 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.Composite.Punto;
+
 import java.util.ArrayList;
 
 public interface Pregunta {
-    Integer puntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError;
+    Punto puntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError;
     void iniciar(Jugador jugador) throws PreguntaError;
     void seleccionarOpcion(Opcion opcion) throws PreguntaError, RespuestaError;
     Respuesta confirmar() throws PreguntaError;
