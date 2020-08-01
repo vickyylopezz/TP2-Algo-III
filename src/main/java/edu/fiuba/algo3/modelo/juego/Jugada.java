435dd5198218702ae036362769dfa906163e785f
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-01 09:13:43 -0300

    Agregados metodos de tiempo en Jugada con sus test.

--- src/main/java/edu/fiuba/algo3/modelo/juego/Jugada.java
+++ src/main/java/edu/fiuba/algo3/modelo/juego/Jugada.java
@@ -2,12 +2,17 @@ package edu.fiuba.algo3.modelo.juego;
 
 import edu.fiuba.algo3.modelo.excepciones.JugadaError;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.util.tiempo.MiliSegundo;
 import edu.fiuba.algo3.modelo.util.tiempo.Tiempo;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Jugada {
 
     private final Pregunta pregunta;
     private final Jugador jugador;
+    private final Respuesta respuesta;
 
     public Jugada(Pregunta pregunta, Jugador jugador) throws JugadaError {
         if (pregunta == null) throw new JugadaError("Pregunta indefinida");
@@ -15,11 +20,28 @@ public class Jugada {
 
         this.pregunta = pregunta;
         this.jugador = jugador;
+        this.respuesta = new Respuesta(pregunta, jugador);
     }
 
     public String tituloPregunta() { return this.pregunta.obtenerTitulo(); }
 
     public Tiempo tiempoPregunta() { return this.pregunta.obtenerTiempo(); }
+
+    public Tiempo tiempoTrancurrido() throws JugadaError {
+        if (!this.respuesta.abierta()) {
+            throw new JugadaError("El tiempo no ha sido inicializado");
+        }
+        Tiempo tiempoActual = MiliSegundo.crearConFecha(new Date());
+        return tiempoActual.restar(this.respuesta.obtenerTiempoInicial());
+    }
+
+    public void iniciarTiempo() {
+        this.respuesta.abrir();
+    }
+
+    public Tiempo tiempoRestante() throws JugadaError {
+        return this.pregunta.obtenerTiempo().restar(this.tiempoTrancurrido());
+    }
 
     // tituloPregunta() -> String
     // tiempoPregunta() -> Tiempo
@@ -31,7 +53,7 @@ public class Jugada {
 
         // opciones() -> []Opcion
         // comodines() -> []Comodin
-        // comdoinesValidos() -> []Comodin
+        // comodoinesValidos() -> []Comodin
 
         // seleccionarOpcion(Opcion) -> nil
         // aplicarComodin(Comodin) -> nil
--- src/main/test/edu/fiuba/algo3/modelo/juego/JugadaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/juego/JugadaTest.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo.juego;
 import edu.fiuba.algo3.modelo.excepciones.JugadaError;
 import edu.fiuba.algo3.modelo.excepciones.RespuestaError;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.util.tiempo.MiliSegundo;
 import edu.fiuba.algo3.modelo.util.tiempo.Segundo;
 import edu.fiuba.algo3.modelo.util.tiempo.Tiempo;
 import org.junit.jupiter.api.Test;
@@ -60,5 +61,92 @@ public class JugadaTest {
         Jugada jugada = new Jugada(pregunta, jugador);
 
         assertEquals(seg.valor(), jugada.tiempoPregunta().segundos().valor());
+    }
+
+    @Test
+    public void tiempoTrancurridoSinIniciarTiempoLanzaJugadaError() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        assertThrows(JugadaError.class, jugada::tiempoTrancurrido);
+    }
+
+    @Test
+    public void tiempoTranscurridoConTiempoIniciadoNoLanzaJugadaError() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        jugada.iniciarTiempo();
+
+        assertDoesNotThrow(jugada::tiempoTrancurrido);
+    }
+
+    //@Test
+    public void tiempoTrancurridoEsElTiempoCorrecto() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        jugada.iniciarTiempo();
+
+        try { Thread.sleep(1000L); }
+        catch (InterruptedException e) { fail(); }
+
+        MiliSegundo ms = jugada.tiempoTrancurrido().miliSegundos();
+
+        int maxErrorTiempo = 50;
+        long variacionTiempo = Math.abs(ms.valor() - 1000L);
+        assertTrue(variacionTiempo < maxErrorTiempo);
+    }
+
+    @Test
+    public void tiempoRestanteSinIniciarTiempoLanzaJugadaError() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        assertThrows(JugadaError.class, jugada::tiempoRestante);
+    }
+
+    @Test
+    public void tiempoRestanteConTiempoIniciadoNoLanzaJugadaError() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        when(pregunta.obtenerTiempo()).thenReturn(Segundo.crearConLiteral(5L));
+
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        jugada.iniciarTiempo();
+
+        assertDoesNotThrow(jugada::tiempoRestante);
+    }
+
+    //@Test
+    public void tiempoRestanteEsElTiempoCorrecto() throws JugadaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        when(pregunta.obtenerTiempo()).thenReturn(Segundo.crearConLiteral(5L));
+
+        Jugador jugador = mock(Jugador.class);
+
+        Jugada jugada = new Jugada(pregunta, jugador);
+
+        jugada.iniciarTiempo();
+
+        try { Thread.sleep(1000L); }
+        catch (InterruptedException e) { fail(); }
+
+        MiliSegundo ms = jugada.tiempoRestante().miliSegundos();
+
+        // tiempos en milisegundos
+        int maxErrorTiempo = 50;
+        long variacionTiempo = Math.abs(ms.valor() - 4000L);
+        assertTrue(variacionTiempo < maxErrorTiempo);
     }
 }
