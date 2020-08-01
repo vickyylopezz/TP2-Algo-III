e080f8ed285429570078d3c46fdd5214f2830872
celedituro <cdituro@fi.uba.ar>
2020-08-01 11:44:01 -0300

    Agrego tests en clase MultiplicadorTest

--- src/main/test/edu/fiuba/algo3/modelo/juego/MultiplicadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/juego/MultiplicadorTest.java
@@ -6,8 +6,7 @@ import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 import edu.fiuba.algo3.modelo.util.punto.PuntoPositivo;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 public class MultiplicadorTest {
@@ -29,4 +28,30 @@ public class MultiplicadorTest {
 
         assertEquals(2,multiplicador.factor());
     }
+
+    /*@Test
+    public void seAplicaSobrePreguntaConPenalidadYComodinSeGuardaEnRespuesta() throws ComodinError, RespuestaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Multiplicador multiplicador = new Multiplicador(2);
+        Jugador jugador = new Jugador("JUan");
+        Respuesta respuesta = new Respuesta(pregunta,jugador);
+        Opcion opcionCorrecta = new Opcion("Bien",new PuntoPositivo());
+        respuesta.agregarOpcion(opcionCorrecta);
+
+        multiplicador.asignarA(respuesta);
+
+        assertEquals(1,respuesta.comodines().size());
+    }
+
+    @Test
+    public void seAplicaSobrePreguntaSinPenalidadYSeLanzaExcepcion() throws ComodinError, RespuestaError {
+        Pregunta pregunta = mock(Pregunta.class);
+        Multiplicador multiplicador = new Multiplicador(2);
+        Jugador jugador = new Jugador("JUan");
+        Respuesta respuesta = new Respuesta(pregunta,jugador);
+        Opcion opcionCorrecta = new Opcion("Bien",new PuntoPositivo());
+        respuesta.agregarOpcion(opcionCorrecta);
+
+        assertThrows(ComodinError.class, () ->  multiplicador.asignarA(respuesta));
+    }*/
 }
