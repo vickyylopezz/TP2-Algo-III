835ed49dcddf708a41e4afd9b008b891fb509e97
celedituro <cdituro@fi.uba.ar>
2020-08-02 16:43:26 -0300

    Agrego estados de Penalidad

---
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/estados/ConPenalidad.java
@@ -0,0 +1,8 @@
+package edu.fiuba.algo3.modelo.preguntas.estados;
+
+public class ConPenalidad implements Penalidad{
+    @Override
+    public Boolean conPenalidad() {
+        return true;
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/estados/Penalidad.java
@@ -0,0 +1,5 @@
+package edu.fiuba.algo3.modelo.preguntas.estados;
+
+public interface Penalidad {
+    Boolean conPenalidad();
+}
