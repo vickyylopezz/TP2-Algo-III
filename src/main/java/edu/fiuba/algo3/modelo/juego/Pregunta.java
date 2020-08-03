5fb25bc1dd0c2581a05e6c4998d95ba1219c2087
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-03 16:30:51 -0300

    Actualizacion clase abstracta pregunta.

--- src/main/java/edu/fiuba/algo3/modelo/juego/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/juego/Pregunta.java
@@ -1,13 +1,8 @@
 package edu.fiuba.algo3.modelo.juego;
 
-import edu.fiuba.algo3.modelo.excepciones.PreguntaError;
-import edu.fiuba.algo3.modelo.excepciones.RespuestaError;
-import edu.fiuba.algo3.modelo.juego.Jugador;
-import edu.fiuba.algo3.modelo.juego.Opcion;
-import edu.fiuba.algo3.modelo.juego.Respuesta;
-import edu.fiuba.algo3.modelo.preguntas.estados.Penalidad;
+import edu.fiuba.algo3.modelo.juego.opcion.Opcion;
+import edu.fiuba.algo3.modelo.preguntas.estados.EstadoPregunta;
 import edu.fiuba.algo3.modelo.util.punto.Punto;
-import edu.fiuba.algo3.modelo.util.punto.PuntoPositivo;
 
 import java.util.ArrayList;
 
@@ -15,34 +10,38 @@ public abstract class Pregunta {
 
     private final String titulo;
     private final ArrayList<Opcion> opciones = new ArrayList<>();
-    protected final Penalidad estadoPenalidad;
-    protected ArrayList<Opcion> opcionesCorrectas = new ArrayList<>();
-    /* protected final Calculador calculadorPuntaje = null;*/
+    protected final EstadoPregunta estado;
 
-    protected Pregunta(String titulo, Penalidad penalidad){
+    protected Pregunta(String titulo, EstadoPregunta estado){
         this.titulo = titulo;
-        this.estadoPenalidad = penalidad;
+        this.estado = estado;
     }
-
-    public abstract Punto puntajeConOpciones(ArrayList<Opcion> opciones) throws PreguntaError;
-    //protected abstract void iniciar(Jugador jugador) throws PreguntaError;
-    //public abstract void seleccionarOpcion(Opcion opcion) throws PreguntaError, RespuestaError;
-    //abstract Respuesta confirmar() throws PreguntaError;
 
     public String obtenerTitulo(){
         return this.titulo;
     }
 
+    public Boolean conPenalidad() { return this.estado.conPenalidad(); }
+
+    public Punto puntajeCorrecto() { return this.estado.puntajeCorrecto(); }
+
+    public Punto puntajeIncorrecto() { return this.estado.puntajeIncorrecto(); }
+
+    public Punto puntajeConOpciones(ArrayList<Opcion> opciones) {
+        return this.estado.puntajeConOpciones(this, opciones);
+    }
+
+    public Boolean opcionesCorrectas(ArrayList<Opcion> opciones) {
+        return this.estado.opcionesCorrectas(this, opciones);
+    }
+
     public ArrayList<Opcion> obtenerOpciones(){
-        return this.opciones;
+        return new ArrayList<>(this.opciones);
     }
 
-    public ArrayList<Opcion> obtenerOpcionesCorrectas(){
-        return this.opcionesCorrectas;
+    public ArrayList<Opcion> opcionesSeleccionables(ArrayList<Opcion> seleccionadas) {
+        ArrayList<Opcion> seleccionables = this.obtenerOpciones();
+        for (Opcion opcion: seleccionadas) seleccionables.remove(opcion);
+        return seleccionables;
     }
-
-    public boolean conPenalizacion() {
-        return this.estadoPenalidad.conPenalidad();
-    }
-
 }
