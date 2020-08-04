ed119da9c82d06519f104e89989f0d38f87916ab
celedituro <cdituro@fi.uba.ar>
2020-08-04 01:27:13 -0300

    Arreglo nombres

--- src/main/test/edu/fiuba/algo3/modelo/juego/OpcionClasicaTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/juego/OpcionClasicaTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OpcionClasicaTest {
     @Test
     public void ObtenerTituloDevuelveElTituloDeLaOpcion(){
-        Opcion opcion = new OpcionClasica("Una opcion", new PuntoPositivo());
+        OpcionClasica opcion = new OpcionClasica("Una opcion", new PuntoPositivo());
         assertEquals("Una opcion",opcion.obtenerTitulo());
     }
 
@@ -19,7 +19,7 @@ public class OpcionClasicaTest {
     public void ObtenerPuntoDevuelveElPuntoDeLaOpcion(){
         Punto puntoPositivo = new PuntoPositivo();
 
-        Opcion opcion = new OpcionClasica("Otra opcion", puntoPositivo);
+        OpcionClasica opcion = new OpcionClasica("Otra opcion", puntoPositivo);
 
         assertEquals(puntoPositivo ,opcion.obtenerPunto());
     }
