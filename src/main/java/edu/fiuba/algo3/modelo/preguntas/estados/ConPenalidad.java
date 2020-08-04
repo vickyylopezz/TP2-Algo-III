dfc7393759f218414f63347ae2c7c1f22481c700
vickyylopezz <vlopez@fi.uba.ar>
2020-08-04 00:18:39 -0300

    Arreglo bug de ConPenalidad donde el puntaje incorrecto debia ser PuntoNegativo y era PuntoNulo

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/estados/ConPenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/estados/ConPenalidad.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.modelo.preguntas.estados;
 
 import edu.fiuba.algo3.modelo.preguntas.calculadorPuntaje.CalculadorPuntaje;
 import edu.fiuba.algo3.modelo.util.punto.Punto;
+import edu.fiuba.algo3.modelo.util.punto.PuntoNegativo;
 import edu.fiuba.algo3.modelo.util.punto.PuntoNulo;
 
 import java.util.ArrayList;
@@ -16,5 +17,5 @@ public class ConPenalidad extends EstadoPregunta {
     public Boolean conPenalidad() { return true; }
 
     @Override
-    public Punto puntajeIncorrecto() { return new PuntoNulo(); }
+    public Punto puntajeIncorrecto() { return new PuntoNegativo(); }
 }
