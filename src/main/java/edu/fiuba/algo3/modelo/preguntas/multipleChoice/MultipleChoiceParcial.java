0c33be24b5b0fad2030bc10f33e453048de24775
vickyylopezz <vlopez@fi.uba.ar>
2020-08-06 14:31:59 -0300

    Cambio excepciones de MultipleChoice

--- src/main/java/edu/fiuba/algo3/modelo/preguntas/multipleChoice/MultipleChoiceParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/preguntas/multipleChoice/MultipleChoiceParcial.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.modelo.preguntas.multipleChoice;
 
+import edu.fiuba.algo3.modelo.excepciones.CantidadMaximaDeOpcionesError;
 import edu.fiuba.algo3.modelo.excepciones.PreguntaError;
 import edu.fiuba.algo3.modelo.juego.Pregunta;
 import edu.fiuba.algo3.modelo.preguntas.estados.SinPenalidad;
@@ -19,7 +20,7 @@ public class MultipleChoiceParcial extends Pregunta {
 
     public void agregarOpcionCorrecta(String opcionTitulo) throws PreguntaError {
         if (this.opciones.size() == 5){
-            throw new PreguntaError("Se alcanzo el maximo de opciones para esta pregunta");
+            throw new CantidadMaximaDeOpcionesError();
         }
         Opcion opcion = new OpcionClasica(opcionTitulo, this.puntajeCorrecto());
         this.opciones.add(opcion);
@@ -27,7 +28,7 @@ public class MultipleChoiceParcial extends Pregunta {
 
     public void agregarOpcionIncorrecta(String opcionTitulo) throws PreguntaError {
         if (this.opciones.size() == 5){
-            throw new PreguntaError("Se alcanzo el maximo de opciones para esta pregunta");
+            throw new CantidadMaximaDeOpcionesError();
         }
         Opcion opcion = new OpcionClasica(opcionTitulo, this.puntajeIncorrecto());
         this.opciones.add(opcion);
