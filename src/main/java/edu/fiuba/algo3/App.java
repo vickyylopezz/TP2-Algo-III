eb71ee0685eac7bf51ef07fbcb6c1bc311b40035
floritfran <fflorit@fi.uba.ar>
2020-08-09 13:32:28 -0300

    PreviaPregunta por ahora

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,18 +1,44 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.modelo.juego.Jugada;
+import edu.fiuba.algo3.modelo.juego.Jugador;
+import edu.fiuba.algo3.modelo.preguntas.multipleChoice.MultipleChoiceClasico;
 import edu.fiuba.algo3.vista.escenas.CargarPreguntasVista;
 import edu.fiuba.algo3.vista.escenas.Escena;
+import edu.fiuba.algo3.vista.escenas.PreviaPreguntaVista;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
         Escena cargadorPreguntas = new CargarPreguntasVista();
+        MultipleChoiceClasico pregunta = new MultipleChoiceClasico("¿Cuál es el apellido de nuestro corrector?");
+        Jugador jugador = new Jugador("Carlos");
+        Jugada jugada = new Jugada(pregunta, jugador);
+        Escena previaPregunta = new PreviaPreguntaVista(jugada);
+
+        /*
+        Timer timer = new Timer();
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                System.exit(0);
+            }
+        };
+        */
 
         stage.setScene(cargadorPreguntas.obtenerEscena());
+        /*
+        stage.setScene(previaPregunta.obtenerEscena());
         stage.show();
+        timer.schedule(tarea, 6000);
+         */
     }
 
     public static void main(String[] args) {
