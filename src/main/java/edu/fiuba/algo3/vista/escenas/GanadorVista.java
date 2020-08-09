abe1dd6cb9565d123c90cb5132c3fc4aa5e9cbba
vickyylopezz <vlopez@fi.uba.ar>
2020-08-09 18:00:19 -0300

    Agrego cosas a GanadorVista

--- src/main/java/edu/fiuba/algo3/vista/escenas/GanadorVista.java
+++ src/main/java/edu/fiuba/algo3/vista/escenas/GanadorVista.java
@@ -17,8 +17,7 @@ import javafx.scene.layout.VBox;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import static javafx.geometry.Pos.TOP_CENTER;
-import static javafx.geometry.Pos.TOP_LEFT;
+import static javafx.geometry.Pos.*;
 
 public class GanadorVista extends EstructuraPrincipalVista{
     @Override
@@ -33,7 +32,10 @@ public class GanadorVista extends EstructuraPrincipalVista{
 
     @Override
     protected Node centro() {
-        StackPane etiqueta = new StackPane();
+        VBox vbox1 = new VBox();
+        vbox1.setStyle("-fx-border-radius: 2;-fx-border-color: grey; -fx-background-radius: 2; -fx-background-color: white");
+
+        /*StackPane etiqueta = new StackPane();
         etiqueta.setMaxHeight(20);
         etiqueta.setMaxWidth(150);
         etiqueta.setPadding(new Insets(90,50,90,50));
@@ -52,13 +54,44 @@ public class GanadorVista extends EstructuraPrincipalVista{
 
 
 
-        VBox contenedorHorizontal = new VBox(jugador);
+        //VBox contenedorHorizontal = new VBox(jugador);
 
-        etiqueta.getChildren().add(contenedorHorizontal);
-        contenedorHorizontal.setTranslateY(-50);
+        //etiqueta.getChildren().add(contenedorHorizontal);
+        //contenedorHorizontal.setTranslateY(-50);
+
+*/
+        Group contenedorIcono1 = new Group();
+        try {
+            FileInputStream stream = new FileInputStream("src/main/resources/jugador.png");
+            ImageView icono1 = new ImageView(new Image(stream));
+            icono1.setPreserveRatio(true);
+            icono1.setFitHeight(150);
+
+            contenedorIcono1.getChildren().add(icono1);
+        } catch (FileNotFoundException e) { e.printStackTrace(); }
+
+        vbox1.getChildren().addAll(contenedorIcono1);
+        vbox1.setAlignment(Pos.BOTTOM_CENTER);
+        vbox1.setMaxHeight(20);
+        vbox1.setMaxWidth(150);
+        vbox1.setPadding(new Insets(90,50,90,50));
+
+        Label jLabelTituloGanador = new Label("G A N A D O R");
+        jLabelTituloGanador.setStyle("-fx-text-fill: #9463EB; -fx-font-size: 30; -fx-font-weight: bold");
+        jLabelTituloGanador.setAlignment(TOP_CENTER);
+
+        Label jLabelGanador = new Label("nombre ganador");
+        jLabelGanador.setStyle("-fx-text-fill: #9463EB; -fx-font-size: 30; -fx-font-weight: bold");
 
 
-        return etiqueta;
+        VBox vbox = new VBox(20);
+        //hbox.setPadding(new Insets(100,50,100,50));
+        //vbox.setStyle("-fx-border-radius: 2;-fx-border-color: grey; -fx-background-radius: 2; -fx-background-color: white");
+
+        vbox.getChildren().addAll(jLabelTituloGanador,vbox1,jLabelGanador);
+        vbox.setAlignment(Pos.CENTER);
+        return vbox;
+
     }
 
     @Override
