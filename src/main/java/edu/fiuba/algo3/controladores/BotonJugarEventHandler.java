a6c76aa3ad71a0d85e5f1e1cde91a128e49a7485
celedituro <cdituro@fi.uba.ar>
2020-08-10 09:03:43 -0300

    Agrego controladores de botones

---
+++ src/main/java/edu/fiuba/algo3/controladores/BotonJugarEventHandler.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.controladores;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BotonJugarEventHandler implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
