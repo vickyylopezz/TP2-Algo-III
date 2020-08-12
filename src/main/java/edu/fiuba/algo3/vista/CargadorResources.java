34cd4b25a39a509fd312e104215391ef2ae280b6
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-12 10:26:19 -0300

    Fix bug CargadorResources

--- src/main/java/edu/fiuba/algo3/vista/CargadorResources.java
+++ src/main/java/edu/fiuba/algo3/vista/CargadorResources.java
@@ -5,9 +5,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class CargadorResources {
-    public static Image obtenerImagen(String Path) {
+    public static Image obtenerImagen(String path) {
         FileInputStream stream = null;
-        try { stream = new FileInputStream(Resources.LogoPrincipalRuta()); }
+        try { stream = new FileInputStream(path); }
         catch (FileNotFoundException e) { e.printStackTrace(); }
         if (stream == null) return null;
         return new Image(stream);
