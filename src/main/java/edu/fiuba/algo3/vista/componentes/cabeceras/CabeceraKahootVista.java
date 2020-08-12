a81655d4d5dec7b8806e5c5e9397366ba8f83097
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-12 09:52:20 -0300

    Refactor Cabecera hereda de GridPane

--- src/main/java/edu/fiuba/algo3/vista/componentes/cabeceras/CabeceraKahootVista.java
+++ src/main/java/edu/fiuba/algo3/vista/componentes/cabeceras/CabeceraKahootVista.java
@@ -1,9 +1,8 @@
 package edu.fiuba.algo3.vista.componentes.cabeceras;
 
+import edu.fiuba.algo3.vista.CargadorResources;
 import edu.fiuba.algo3.vista.Resources;
-import edu.fiuba.algo3.vista.componentes.Componente;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
@@ -13,23 +12,23 @@ import javafx.scene.layout.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-public class CabeceraKahootVista implements Componente {
-
-    private GridPane cabecera;
+public class CabeceraKahootVista extends GridPane {
 
     public CabeceraKahootVista(Node nodoIzquierdo, Node nodoDerecho) {
         this.estrucutraCabecera();
+        this.cargarEstilo();
 
         this.contenidoPanelIzquierdo(nodoIzquierdo);
         this.contenidoPanelCentral();
         this.contenidoPanelDerecho(nodoDerecho);
     }
 
-    private void estrucutraCabecera() {
-        this.cabecera = new GridPane();
-        this.cabecera.setPadding(new Insets(10, 10, 10, 10));
-        this.cabecera.setAlignment(Pos.CENTER);
+    private void cargarEstilo() {
+        this.setPadding(new Insets(10));
+        this.setAlignment(Pos.CENTER);
+    }
 
+    private void estrucutraCabecera() {
         ColumnConstraints col1 = new ColumnConstraints();
         ColumnConstraints col2 = new ColumnConstraints();
         ColumnConstraints col3 = new ColumnConstraints();
@@ -38,7 +37,7 @@ public class CabeceraKahootVista implements Componente {
         col2.setPercentWidth(40);
         col3.setPercentWidth(30);
 
-        this.cabecera.getColumnConstraints().addAll(col1, col2, col3);
+        this.getColumnConstraints().addAll(col1, col2, col3);
     }
 
     private void contenidoPanelIzquierdo(Node nodo) {
@@ -46,10 +45,9 @@ public class CabeceraKahootVista implements Componente {
 
         StackPane columna = new StackPane();
         columna.setAlignment(Pos.CENTER_LEFT);
-
         columna.getChildren().add(nodo);
 
-        this.cabecera.add(columna, 0, 0);
+        this.add(nodo, 0, 0);
     }
 
     private void contenidoPanelDerecho(Node nodo) {
@@ -57,30 +55,21 @@ public class CabeceraKahootVista implements Componente {
 
         StackPane columna = new StackPane();
         columna.setAlignment(Pos.CENTER_RIGHT);
-
         columna.getChildren().add(nodo);
 
-        this.cabecera.add(columna, 2, 0);
+        this.add(columna, 2, 0);
     }
 
     private void contenidoPanelCentral() {
-        FileInputStream stream = null;
-        try { stream = new FileInputStream(Resources.LogoPrincipalRuta()); }
-        catch (FileNotFoundException e) { e.printStackTrace(); }
-        if (stream == null) return;
-
         StackPane columna = new StackPane();
         columna.setAlignment(Pos.CENTER);
 
-        ImageView logo = new ImageView(new Image(stream));
+        Image img = CargadorResources.obtenerImagen(Resources.LogoPrincipalRuta());
+        ImageView logo = new ImageView(img);
         logo.setPreserveRatio(true);
         logo.setFitHeight(75);
-
         columna.getChildren().add(logo);
 
-        this.cabecera.add(columna, 1, 0);
+        this.add(columna, 1, 0);
     }
-
-    @Override
-    public Node obtenerNodo() { return this.cabecera; }
 }
