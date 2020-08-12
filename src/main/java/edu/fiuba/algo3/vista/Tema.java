61fb55a0d3fa2557a179940529fe6d2700d4957f
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-12 09:48:31 -0300

    Creacion de clase Tema para los colores.

---
+++ src/main/java/edu/fiuba/algo3/vista/Tema.java
@@ -0,0 +1,14 @@
+package edu.fiuba.algo3.vista;
+
+import javafx.scene.paint.Color;
+
+public class Tema {
+    static public Color colorBotonPrincipal = Color.VIOLET;
+    static public Color colorBotonPrincipalDesactivado = Color.VIOLET;
+
+    static public Color colorBotonSegundario = Color.VIOLET;
+    static public Color colorBotonSegundarioDesactivado = Color.VIOLET;
+
+    static public Color colorBotonTerceario = Color.VIOLET;
+    static public Color colorBotonTercearioDesactivado = Color.VIOLET;
+}
