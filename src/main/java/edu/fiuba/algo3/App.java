c25cd7f58d60639ffa1687c5d87de4f17bbe616d
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-15 18:28:33 -0300

    Separando rama Develop para parte del modelo, sin GUI

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,44 +1,13 @@
 package edu.fiuba.algo3;
 
-import edu.fiuba.algo3.controladores.IniciarJuegoControlador;
-import edu.fiuba.algo3.modelo.excepciones.preguntas.PreguntaError;
-import edu.fiuba.algo3.modelo.juego.Jugada;
-import edu.fiuba.algo3.modelo.juego.Jugador;
-import edu.fiuba.algo3.modelo.preguntas.verdaderoFalso.VerdaderoFalsoConPenalidad;
-import edu.fiuba.algo3.vista.escenas.juego.PreviaPreguntaVista;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException, PreguntaError {
-        //Escena crearJuego = new CrearJuegoVista();
-        //Escena cargadorPreguntas = new CargarPreguntasVista();
-        //Escena iniciarJuego = new IniciarJuegoVista();
-        //Escena registrarJugadores = new RegistrarJugadoresVista();
-        //Escena puntosobtenidos = new PuntosObtenidosVista();
-        //Escena ganador = new GanadorVista();
-
-        //Jugada jugada = new Jugada(new VerdaderoFalsoConPenalidad("Estamos en el año 2021"), new Jugador("Carlos"));
-        //Escena previaPregunta = new PreviaPreguntaVista(jugada);
-
-        //stage.setScene(crearJuego.obtenerEscena());
-        //stage.setScene(cargadorPreguntas.obtenerEscena());
-        //stage.setScene(iniciarJuego.obtenerEscena());
-        //stage.setScene(registrarJugadores.obtenerEscena());
-        //stage.setScene(puntosobtenidos.obtenerEscena());
-        //stage.setScene(previaPregunta.obtenerEscena());
-        //stage.setScene(ganador.obtenerEscena());
-        //stage.show();
-
-        //CargarPreguntaControlador preguntasControlador = new CargarPreguntaControlador(stage);
-        //preguntasControlador.mostrarVista();
-
-        IniciarJuegoControlador iniciarJuegoControlador = new IniciarJuegoControlador(stage);
-        iniciarJuegoControlador.mostrarVista();
+    public void start(Stage stage) {
+        System.out.println("Rama sin interface");
     }
 
     public static void main(String[] args) {
