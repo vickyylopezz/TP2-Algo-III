7b654f1687334b0f23fe9b375a0336c321668552
vickyylopezz <vlopez@fi.uba.ar>
2020-08-17 11:50:01 -0300

    Agrego Juego a Kahoot y KahootModo para poder recibirlo desde KahootJuego en KahootResultados

--- src/main/java/edu/fiuba/algo3/kahoot/KahootResultados.java
+++ src/main/java/edu/fiuba/algo3/kahoot/KahootResultados.java
@@ -1,20 +1,26 @@
 package edu.fiuba.algo3.kahoot;
 
+import edu.fiuba.algo3.eventos.kahoot.CambioEscenaEventHandler;
+import edu.fiuba.algo3.modelo.excepciones.punto.PuntoError;
+import edu.fiuba.algo3.modelo.juego.Juego;
 import edu.fiuba.algo3.modelo.juego.Jugador;
 import edu.fiuba.algo3.modelo.juego.Pregunta;
+import edu.fiuba.algo3.vista.escenas.resultados.UltimaEscena;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
 public class KahootResultados extends KahootModo {
-
-    public KahootResultados(Stage stage, ArrayList<Pregunta> preguntas, ArrayList<Jugador> jugadores) {
-        super(stage, preguntas, jugadores);
+    public KahootResultados(Stage stage, ArrayList<Pregunta> preguntas, ArrayList<Jugador> jugadores, Juego juego) {
+        super(stage, preguntas, jugadores,juego);
     }
 
     @Override
-    public void iniciar(MediaPlayer reproductor) {
+    public void iniciar(MediaPlayer reproductor) throws PuntoError {
+        UltimaEscena finalizacion = new UltimaEscena(reproductor,this.jugadores.get(0),this.jugadores.get(1),this.juego);
+        finalizacion.eventoSiguiente(this.eventoSalida,"Salir");
 
+        new CambioEscenaEventHandler(this.stage, finalizacion).handle(null);
     }
 }
