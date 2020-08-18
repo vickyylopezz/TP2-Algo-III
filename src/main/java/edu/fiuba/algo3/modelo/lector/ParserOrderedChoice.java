eee8026e705bf1e46b6fa22ddd3afc9312443109
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-18 09:03:41 -0300

    Agregando robustes y test a ParserOrderedChoice.

--- src/main/java/edu/fiuba/algo3/modelo/lector/ParserOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/lector/ParserOrderedChoice.java
@@ -3,18 +3,41 @@ package edu.fiuba.algo3.modelo.lector;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import edu.fiuba.algo3.modelo.excepciones.lector.LectorFormatoDePreguntaError;
 import edu.fiuba.algo3.modelo.excepciones.preguntas.PreguntaError;
 import edu.fiuba.algo3.modelo.juego.Pregunta;
 import edu.fiuba.algo3.modelo.preguntas.orderedChoice.OrderedChoice;
 
 public class ParserOrderedChoice implements ParserPregunta {
     @Override
-    public Pregunta parsear(JsonObject objeto) throws PreguntaError {
-        String tituloPregunta = objeto.get("pregunta").getAsString();
-        OrderedChoice pregunta = new OrderedChoice(tituloPregunta);
+    public Pregunta parsear(JsonObject objeto) throws PreguntaError, LectorFormatoDePreguntaError {
+        JsonElement tipoJson = objeto.get("tipo");
+        if (tipoJson == null) {
+            throw new LectorFormatoDePreguntaError("Tipo de pregunta no definido");
+        }
+        String tipo = tipoJson.getAsString();
+        if (!tipo.equals("Ordered")) {
+            throw new LectorFormatoDePreguntaError("Tipo pregunta invalido");
+        }
 
-        JsonArray orden = objeto.getAsJsonArray("orden");
-        for (JsonElement opcion: orden){
+        JsonElement preguntaJson = objeto.get("pregunta");
+        if (preguntaJson == null) {
+            throw new LectorFormatoDePreguntaError("Atributo 'pregunta' no definido");
+        }
+        String nombrePregunta = preguntaJson.getAsString();
+
+        OrderedChoice pregunta = new OrderedChoice(nombrePregunta);
+
+        JsonArray ordenOpcionesJson;
+        try { ordenOpcionesJson = objeto.getAsJsonArray("orden"); }
+        catch (ClassCastException e) {
+            throw new LectorFormatoDePreguntaError("Array de opciones tipo incorrecto");
+        }
+        if (ordenOpcionesJson == null) {
+            throw new LectorFormatoDePreguntaError("Array de opciones indefinido");
+        }
+
+        for (JsonElement opcion: ordenOpcionesJson){
             pregunta.agregarOpcion(opcion.getAsString());
         }
 
