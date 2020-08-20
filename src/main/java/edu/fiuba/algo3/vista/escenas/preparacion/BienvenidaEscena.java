04ed1efc7b5ea9f10502f6f07304a42194087670
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-20 10:35:53 -0300

    Refactor Escena Bienvenida

--- src/main/java/edu/fiuba/algo3/vista/escenas/preparacion/BienvenidaEscena.java
+++ src/main/java/edu/fiuba/algo3/vista/escenas/preparacion/BienvenidaEscena.java
@@ -1,11 +1,17 @@
 package edu.fiuba.algo3.vista.escenas.preparacion;
 
+import edu.fiuba.algo3.vista.Tema;
 import edu.fiuba.algo3.vista.componentes.botones.BotonCuadradoVista;
 import edu.fiuba.algo3.vista.escenas.BaseEscena;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Paint;
 
 public class BienvenidaEscena extends BaseEscena {
 
@@ -21,8 +27,14 @@ public class BienvenidaEscena extends BaseEscena {
     public void eventoBotonPrincipal(EventHandler<ActionEvent> evento) {
         if (evento == null) this.cuerpo.getChildren().clear();
 
-        String tituloBoton = "Crear Jugadores";
-        BotonCuadradoVista boton = new BotonCuadradoVista(tituloBoton);
+        BotonCuadradoVista boton = new BotonCuadradoVista("Crear Jugadores");
+        boton.setTextFill(Tema.ColorBotonSegundarioTexto);
+        boton.setBackground(new Background(new BackgroundFill(
+                Tema.colorBotonSegundario,
+                new CornerRadii(5),
+                Insets.EMPTY
+        )));
+
         boton.setOnAction(evento);
 
         this.cuerpo.getChildren().add(boton);
