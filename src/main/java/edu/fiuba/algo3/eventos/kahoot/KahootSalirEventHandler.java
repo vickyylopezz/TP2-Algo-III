a278ccdf0746d288eca1b21df5e254f000aabfe7
vickyylopezz <vlopez@fi.uba.ar>
2020-08-20 11:06:37 -0300

    Cambio en ultima escena "salir" por "reiniciar"

--- src/main/java/edu/fiuba/algo3/eventos/kahoot/KahootSalirEventHandler.java
+++ src/main/java/edu/fiuba/algo3/eventos/kahoot/KahootSalirEventHandler.java
@@ -1,11 +1,24 @@
 package edu.fiuba.algo3.eventos.kahoot;
 
+import edu.fiuba.algo3.kahoot.Kahoot;
+import edu.fiuba.algo3.modelo.excepciones.punto.PuntoError;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.stage.Stage;
 
 public class KahootSalirEventHandler implements EventHandler<ActionEvent> {
+    private Stage stage;
+    public KahootSalirEventHandler(Stage stage) {
+       this.stage = stage;
+    }
+
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.exit(0);
+        Kahoot app = new Kahoot(stage);
+        try {
+            app.iniciar();
+        } catch (PuntoError puntoError) {
+            puntoError.printStackTrace();
+        }
     }
 }
