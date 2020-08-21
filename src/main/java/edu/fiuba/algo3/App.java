27eb0140c66a902635089306fd830355c8e6a328
alevargas-gif <avargas@fi.uba.ar>
2020-08-21 18:54:22 -0300

    Implementación inicial de resolución dinámica de pantalla

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,15 +1,21 @@
 package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.kahoot.Kahoot;
+import edu.fiuba.algo3.modelo.excepciones.punto.PuntoError;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws PuntoError {
         stage.setMinHeight(800);
-        stage.setMinWidth(1280);
+        stage.setMinWidth(600);
+        Scene escenaPrincipal = new Scene(new Label(), 800, 600);
+        stage.setScene(escenaPrincipal);
+        stage.setMaximized(true);
 
         Kahoot app = new Kahoot(stage);
         app.iniciar();
