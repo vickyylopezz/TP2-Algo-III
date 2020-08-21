f4c9da2bbdf49f7b5f44eafd5ab226bd173d654b
Juan Ignacio Coronel <jcoronel@fi.uba.ar>
2020-08-21 16:31:36 -0300

    Arreglando Merge con develop, actualizacion del Modelo clase App

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,15 +1,13 @@
 package edu.fiuba.algo3;
 
 import edu.fiuba.algo3.kahoot.Kahoot;
-import edu.fiuba.algo3.modelo.excepciones.punto.PuntoError;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws PuntoError {
+    public void start(Stage stage) {
         stage.setMinHeight(800);
         stage.setMinWidth(1280);
 
